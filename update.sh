132b59a7a9c160992939711528481a802b018a00
sirekanian <sirekanian@pm.me>
2022-06-15 20:15:05 +0300

    added tags

--- update.sh
+++ update.sh
@@ -3,7 +3,10 @@
 set -e
 
 wget -qO- "https://raw.githubusercontent.com/sirekanian/list-of-war-enablers/master/list-of-war-enablers.json" \
-  > warmongr/data.json
+  >warmongr/list-of-war-enablers.json
+
+wget -qO- "https://raw.githubusercontent.com/sirekanian/list-of-war-enablers/master/input/data.txt" \
+  >warmongr/list-of-war-enablers.txt
 
 echo "{\"date\":\"$(date -I)\"}" \
-  > warmongr/meta.json
+  >warmongr/meta.json
